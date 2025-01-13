ee19cf867623f343c270eeb58bb0b411b03e4d78
moukito <theofremont50@gmail.com>
2025-01-13 15:18:14 +0100

    Add grayscale conversion functionality
    Introduced a new function `Grayscale` in `imageUtils/greyscale.go` that converts an image to grayscale using the standard luminance formula. This functionality will enable processing images in grayscale format for various use cases.

---
+++ imageUtils/greyscale.go
@@ -0,0 +1,23 @@
+package imageUtils
+
+import (
+	"image"
+	"image/color"
+)
+
+// Grayscale converts an image to grayscale.
+func Grayscale(img image.Image) *image.Gray {
+	bounds := img.Bounds()
+	grayImage := image.NewGray(bounds)
+
+	for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
+		for x := bounds.Min.X; x < bounds.Max.X; x++ {
+			r, g, b, _ := img.At(x, y).RGBA()
+			// Standard luminance formula: 0.299*R + 0.587*G + 0.114*B
+			grayValue := uint8((299*r + 587*g + 114*b) / 1000 >> 8)
+			grayImage.SetGray(x, y, color.Gray{Y: grayValue})
+		}
+	}
+
+	return grayImage
+}
