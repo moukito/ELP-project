40f07bc0944f349e1f54c9abfe65280b123bf6a7
moukito <theofremont50@gmail.com>
2025-01-23 19:59:57 +0100

    Refactor grayscale conversion for improved precision
    Revised the luminance calculation to use float64 for better accuracy in grayscale conversion. Simplified the RGBA to 8-bit conversion process to maintain code clarity and correctness.

--- go/internal/imageUtils/grayScale.go
+++ go/internal/imageUtils/grayScale.go
@@ -13,8 +13,10 @@ func Grayscale(img image.Image) *image.Gray {
 	for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
 		for x := bounds.Min.X; x < bounds.Max.X; x++ {
 			r, g, b, _ := img.At(x, y).RGBA()
-			// Standard luminance formula: 0.299*R + 0.587*G + 0.114*B
-			grayValue := uint8((299*r + 587*g + 114*b) / 1000 >> 8)
+			r8, g8, b8 := r>>8, g>>8, b>>8 // Convert to 8-bit
+			grayValue := uint8(0.299*float64(r8) + 0.587*float64(g8) + 0.114*float64(b8))
+
+			// Updating the pixel in the grayscale image.
 			grayImage.SetGray(x, y, color.Gray{Y: grayValue})
 		}
 	}
