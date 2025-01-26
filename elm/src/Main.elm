f2f49e73dfa5942ad616845e4bd464ad11db4b1c
moukito <theofremont50@gmail.com>
2025-01-26 17:16:35 +0100

    Refactor model and view types to include msg parameter
    Updated the `Model` type to include a `msg` parameter for better type clarity and flexibility. Adjusted associated functions (`init`, `update`, and `view`) to align with the updated type definition. Removed unused code and improved module import organization.

--- elm/src/Main.elm
+++ elm/src/Main.elm
@@ -1,23 +1,26 @@
 module Main exposing (..)
 
 import Browser
-import Html exposing (Html, button, div, text, input)
-import Html.Attributes exposing (value, placeholder)
+import DrawingUtils exposing (display)
+import Html exposing (Html, button, div, input, text)
+import Html.Attributes exposing (placeholder, value)
 import Html.Events exposing (onClick, onInput)
 import ParsingUtils exposing (read)
-import DrawingUtils exposing (display)
-import Svg exposing (Svg)
+import Svg exposing (Svg, svg)
+
 
 
 -- MODEL
 
-type alias Model =
+
+type alias Model msg =
     { code : String
     , error : Maybe String
     , svg : Svg msg
     }
 
-init : Model
+
+init : Model Never
 init =
     { code = ""
     , error = Nothing
@@ -25,13 +28,16 @@ init =
     }
 
 
+
 -- UPDATE
+
 
 type Msg
     = UpdateCode String
     | ParseCode
 
-update : Msg -> Model -> Model
+
+update : Msg -> Model Never -> Model Never
 update msg model =
     case msg of
         UpdateCode newCode ->
@@ -49,21 +55,28 @@ update msg model =
                     }
 
 
+
 -- VIEW
 
-view : Model -> Html Msg
+
+view : Model Never -> Html Msg
 view model =
     div []
         [ input [ placeholder "Enter TcTurtle code", onInput UpdateCode, value model.code ] []
         , button [ onClick ParseCode ] [ text "Parse & Draw" ]
         , case model.error of
-            Nothing -> text ""
-            Just err -> div [] [ text err ]
-        , Html.map (always ()) (Html.fromSvg model.svg)
+            Nothing ->
+                text ""
+
+            Just err ->
+                div [] [ text err ]
+        --, Html.map (always ()) (Html.fromSvg model.svg)
         ]
 
 
+
 -- PROGRAM
+
 
 main =
     Browser.sandbox { init = init, update = update, view = view }
