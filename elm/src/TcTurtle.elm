302f40131de4e0102db4c1cd542da22cea6c0ce8
moukito <theofremont50@gmail.com>
2025-01-26 14:02:56 +0100

    Refactor Program type definition for clarity and consistency.
    The Program type is now explicitly defined as a union type. This enhances readability and aligns with Elm's conventions for type declarations. No functional changes were introduced.

--- elm/src/TcTurtle.elm
+++ elm/src/TcTurtle.elm
@@ -1,9 +1,12 @@
 module TcTurtle exposing (..)
 
+
 type Instruction
     = Forward Int
     | Left Int
     | Right Int
     | Repeat Int (List Instruction)
 
-type Program = List Instruction
+
+type Program
+    = List Instruction
